d7eebb7a277c5f509a49f4cc3e89aa02555a8b41
Ziggy Chen <ziggycs9796@hotmail.com>
2022-10-07 23:11:26 +0800

    feat: promise 练习

---
+++ myPromise/src/selfPromise.js
@@ -0,0 +1,140 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+class MyPromise {
+  constructor(executor) {
+    try {
+      executor(this.resolve, this.reject);
+    } catch (error) {
+      this.rejected(error);
+    }
+  }
+
+  status = PENDING;
+  value = null;
+  reason = null;
+  onFulfilledCallbacks = [];
+  onRejectedCallbacks = [];
+
+  resolve = (value) => {
+    if (this.status === PENDING) {
+      this.status = FULFILLED;
+      this.value = value;
+      while (this.onFulfilledCallbacks.length) {
+        this.onFulfilledCallbacks.shift()(value);
+      }
+    }
+  };
+
+  reject = (reason) => {
+    if (this.status === PENDING) {
+      this.status = REJECTED;
+      this.reason = reason;
+      while (this.onRejectedCallbacks.length) {
+        this.onRejectedCallbacks.shift()(reason);
+      }
+    }
+  };
+
+  then(onFulfilled, onRejected) {
+    const realOnFulfilled =
+      typeof onFulfilled === "function" ? onFulfilled : (val) => val;
+    const realOnRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (reason) => {
+            throw reason;
+          };
+
+    const promise2 = new MyPromise((resolve, reject) => {
+      const fulfilledMicrotask = () => {
+        queueMicrotask(() => {
+          try {
+            const x = realOnFulfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+      const rejectedMicrotask = () => {
+        queueMicrotask(() => {
+          try {
+            const x = realOnRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+
+      if (this.status === FULFILLED) {
+        fulfilledMicrotask();
+      }
+      if (this.status === REJECTED) {
+        rejectedMicrotask();
+      }
+      if (this.status === PENDING) {
+        this.onFulfilledCallbacks.push(fulfilledMicrotask);
+        this.onRejectedCallbacks.push(rejectedMicrotask);
+      }
+    });
+    return promise2;
+  }
+}
+
+function resolvePromise(promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(new TypeError("promise and the value are the same"));
+  }
+
+  if ((typeof x === "function") | (typeof x === "object")) {
+    if (x === null) return resolve(x);
+
+    let then;
+    try {
+      then = x.then;
+    } catch (error) {
+      return reject(error);
+    }
+
+    if (typeof then === "function") {
+      let called = false;
+      try {
+        then.call(
+          x,
+          (y) => {
+            if (called) return;
+            called = true;
+            resolvePromise(promise, y, resolve, reject);
+          },
+          (r) => {
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+      } catch (error) {
+        if (called) return;
+        reject(error);
+      }
+    } else {
+      resolve(x);
+    }
+  } else {
+    resolve(x);
+  }
+}
+
+MyPromise.deferred = function () {
+  var result = {};
+  result.promise = new MyPromise(function (resolve, reject) {
+    result.resolve = resolve;
+    result.reject = reject;
+  });
+
+  return result;
+};
+
+module.exports = MyPromise;
