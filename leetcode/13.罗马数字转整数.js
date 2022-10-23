3c0f0b88c99ea00b684d51f0fae45c08df08d819
Ziggy Chen <ziggycs9796@hotmail.com>
2022-10-23 19:11:27 +0800

    feat: 罗文数字转换整数

---
+++ leetcode/13.罗马数字转整数.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=javascript
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+
+symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+/**
+ * @param {string}
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let value = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (symbols[s[i]] < symbols[s[i + 1]]) {
+      value -= symbols[s[i]];
+    } else {
+      value += symbols[s[i]];
+    }
+  }
+  return value;
+};
+// @lc code=end
