066dc47081fb1826d587e6ee6fc00ba0e290a8c9
Ziggy Chen <ziggycs9796@hotmail.com>
2022-10-23 15:05:32 +0800

    feat: twoSum

---
+++ leetcode/1.两数之和.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const obj = {};
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    if (obj[target - nums[i]] !== undefined) {
+      return [obj[target - nums[i]], i];
+    }
+    obj[nums[i]] = i;
+  }
+};
+// @lc code=end
