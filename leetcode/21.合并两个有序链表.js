3f4d1837a7506c725901204b5aa21ff79cfc267a
Ziggy Chen <ziggycs9796@hotmail.com>
2022-10-25 22:56:48 +0800

    feat: 通过拼接合并两个链表

--- leetcode/21.合并两个有序链表.js
+++ leetcode/21.合并两个有序链表.js
@@ -13,12 +13,24 @@
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-
+var mergeTwoLists = function (l1, l2) {
+  var mergedHead = { val: -1, next: null };
+  crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
+    } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+  return mergedHead.next;
 };
 // @lc code=end
-
